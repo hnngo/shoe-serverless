eea8429bd64de300959b58f5bfceba458c1ad21a
Huynh Nhan Ngo <ngohuynhnhan@gmail.com>
2022-12-24 13:33:48 -0800

    Push project

---
+++ frontend/src/pages/Admin/index.jsx
@@ -0,0 +1,110 @@
+import React, { useEffect } from "react";
+
+import { useStyletron } from "baseui";
+import { useDispatch, useSelector } from "react-redux";
+
+import AdminProducts from "./AdminProducts";
+import AdminAddNewItem from "./AdminAddNewItem";
+import AdminRemoveItem from "./AdminRemoveItem";
+import AdminUpdateItem from "./AdminUpdateItem";
+
+import { HeadingMedium } from "baseui/typography";
+
+import { Container, Row } from "react-grid-system";
+import { shoeActions } from "../../store";
+import Products from "../../components/Blocks/Home/Products";
+
+const Admin = () => {
+  const [css] = useStyletron();
+  const [menuState, setMenuState] = React.useState("items");
+  const [checkAdmin, setCheckAdmin] = React.useState(undefined);
+
+  const state = useSelector((state) => state.products);
+  const category = state["shoes"];
+  const products = category?.products ?? [];
+  const productArray = Object.values(products);
+
+  const dispatch = useDispatch();
+
+  const homeProducts = useSelector((state) => {
+    const products = [];
+    for (const shoe in state.products?.shoes ?? []) {
+      products.push(state.products?.shoes[shoe]);
+      // if (state.products?.shoes[shoe].isListed === 1) {
+
+      // }
+    }
+    return products;
+  });
+
+  useEffect(() => {
+    const isAdmin = localStorage.getItem("is_admin");
+    if (!isAdmin) {
+      alert("Nice try not an Admin: GET OUT");
+      window.location.href = "/";
+      setCheckAdmin(false);
+    } else {
+      setCheckAdmin(true);
+    }
+    dispatch(shoeActions.getAll());
+  }, []);
+
+  if (!checkAdmin) {
+    return null;
+  }
+
+  return (
+    <Container className={css({ maxHeight: "100%" })}>
+      <Row
+        justify="start"
+        className={css({ marginLeft: "-15px", marginRight: "-15px" })}
+      >
+        <HeadingMedium
+          className={css({ marginRight: "1rem", cursor: "pointer" })}
+          onClick={() => setMenuState("items")}
+        >
+          Items
+        </HeadingMedium>
+        <HeadingMedium className={css({ marginRight: "1rem" })}>
+          |
+        </HeadingMedium>
+        <HeadingMedium
+          className={css({ marginRight: "1rem", cursor: "pointer" })}
+          onClick={() => setMenuState("addItem")}
+        >
+          Add item
+        </HeadingMedium>
+
+        <HeadingMedium className={css({ marginRight: "1rem" })}>
+          |
+        </HeadingMedium>
+        {/* <HeadingMedium
+          className={css({ marginRight: "1rem", cursor: "pointer" })}
+          onClick={() => setMenuState("removeItem")}
+        >
+          Remove item
+        </HeadingMedium>
+
+        <HeadingMedium className={css({ marginRight: "1rem" })}>
+          |
+        </HeadingMedium>
+        <HeadingMedium
+          className={css({ marginRight: "1rem", cursor: "pointer" })}
+          onClick={() => setMenuState("updateItem")}
+        >
+          Update item
+        </HeadingMedium> */}
+      </Row>
+      <Row>
+        {menuState === "addItem" && <AdminAddNewItem />}
+        {menuState === "items" && (
+          <AdminProducts productsCategoryName="home" products={homeProducts} />
+        )}
+        {/* {menuState === "removeItem" && <AdminRemoveItem />}
+        {menuState === "updateItem" && <AdminUpdateItem />} */}
+      </Row>
+    </Container>
+  );
+};
+
+export default Admin;
